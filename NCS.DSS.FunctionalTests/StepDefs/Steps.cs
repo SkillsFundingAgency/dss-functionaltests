864ae0f16ba33586e2526f90d685781d7a12fa74
Simon Wren <simon.j.wren@outlook.com>
2019-01-08 08:54:27 +0000

    rename step file in local branch, git ignore change for local app settings

--- NCS.DSS.FunctionalTests/StepDefs/Steps.cs
+++ NCS.DSS.FunctionalTests/StepDefs/Steps.cs
@@ -4,18 +4,20 @@ using FunctionalTests.Models;
 using NCS.DSS.FunctionalTests.Models;
 using Newtonsoft.Json;
 using RestSharp;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
+using System.Configuration;
+using Newtonsoft.Json.Linq;
+
+
 
 namespace FunctionalTests.StepDefs
 {
     [Binding]
-    public class PostSteps
+    public class Steps
     {
         private EnvironmentSettings envSettings = new EnvironmentSettings();
         private readonly RestHelper Rest = new RestHelper();
@@ -34,6 +36,8 @@ namespace FunctionalTests.StepDefs
         private string outcomeId;
         private string webChatId;
 
+
+
         [Given(@"I post a Customer with the following details:")]
         public void GivenIPostACustomerWithTheFollowingDetails(Table table)
         {
@@ -48,13 +52,16 @@ namespace FunctionalTests.StepDefs
             }
         }
 
+
+
+
         [Given(@"I post an adviser with the following details:")]
         public void GivenIPostAnAdviserWithTheFollowingDetails(Table table)
         {
             url = envSettings.BaseUrl + "adviserdetails/api/AdviserDetails";
             var adviser = table.CreateInstance<Adviser>();
             json2 = JsonConvert.SerializeObject(adviser);
-            response = RestHelper.Post(url,json2, envSettings.TouchPointId, envSettings.SubscriptionKey);
+            response = RestHelper.Post(url, json2, envSettings.TouchPointId, envSettings.SubscriptionKey);
             if (response.IsSuccessful)
             {
                 Dictionary<string, string> actualVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
@@ -65,7 +72,7 @@ namespace FunctionalTests.StepDefs
         [Given(@"I post an Interaction with the following details:")]
         public void GivenIPostAnInteractionWithTheFollowingDetails(Table table)
         {
-            url = envSettings.BaseUrl + "interactions/api/Customers/" + customerId +  "/Interactions/";
+            url = envSettings.BaseUrl + "interactions/api/Customers/" + customerId + "/Interactions/";
             var interaction = table.CreateInstance<Interaction>();
             json2 = JsonConvert.SerializeObject(interaction);
             response = RestHelper.Post(url, json2, envSettings.TouchPointId, envSettings.SubscriptionKey);
@@ -108,9 +115,7 @@ namespace FunctionalTests.StepDefs
         [Given(@"I post an ActionPlan with the following details:")]
         public void GivenIPostAnActionPlanWithTheFollowingDetails(Table table)
         {
-            
             url = envSettings.BaseUrl + "actionplans/api/Customers/" + customerId + "/Interactions/" + interactionId + "/ActionPlans/";
-            Console.WriteLine("Test Output");
             var actionPlan = table.CreateInstance<ActionPlan>();
             json2 = JsonConvert.SerializeObject(actionPlan);
             response = RestHelper.Post(url, json2, envSettings.TouchPointId, envSettings.SubscriptionKey);
@@ -230,35 +235,21 @@ namespace FunctionalTests.StepDefs
         public void WhenIPatchTheFollowing(Table table)
         {
             Dictionary<string, string> customer = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
-            customerId = customer.FirstOrDefault().Value;   
-            var x = table.Rows.ToDictionary(r => r["Field"], r => r["Value"]);
+            customerId = customer.FirstOrDefault().Value;
+            Dictionary<string, string> x = table.Rows.ToDictionary(r => r["Field"], r => r["Value"]);
             json2 = JsonConvert.SerializeObject(x);
-            // add the id of the last item to the url for the patch
-            //url = url + "/" + lastId;
-            response = RestHelper.Patch(url,json2, envSettings.TouchPointId, envSettings.SubscriptionKey, customerId);
+            response = RestHelper.Patch(url, json2, envSettings.TouchPointId, envSettings.SubscriptionKey, customerId);
 
         }
 
-        [When(@"I patch the address:")]
-        public void WhenIPatchTheAddress(Table table)
-        {
-            Dictionary<string, string> address = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
-            addressId = address.FirstOrDefault().Value;
-            var x = table.Rows.ToDictionary(r => r["Field"], r => r["Value"]);
-            json2 = JsonConvert.SerializeObject(x);
-            response = RestHelper.Patch(url, json2, envSettings.TouchPointId, envSettings.SubscriptionKey, addressId);
-        }
 
-        [When(@"I patch the ActionPlan:")]
-        public void WhenIPatchTheActionPlan(Table table)
-        {
-            Dictionary<string, string> dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
-            actionPlanId = dict.FirstOrDefault().Value;
-            var x = table.Rows.ToDictionary(r => r["Field"], r => r["Value"]);
-            json2 = JsonConvert.SerializeObject(x);
-            response = RestHelper.Patch(url, json2, envSettings.TouchPointId, envSettings.SubscriptionKey, actionPlanId);
-        }
 
+        //[When(@"I get a Customer by ID")]
+        //public async void WhenIGetACustomerByID()
+        //{
+        //    url = baseUrl + "customers/api/customers/" + customerId;
+        //    response = await RestHelper.GetAsync(url);
+        //}
 
         [When(@"I get a Customer by ID")]
         public void WhenIGetACustomerByID()
@@ -267,12 +258,32 @@ namespace FunctionalTests.StepDefs
             response = RestHelper.Get(url, envSettings.TouchPointId, envSettings.SubscriptionKey);
         }
 
+        [When(@"I search for:")]
+        public void WhenISearchFor(Table table)
+        {
+            Dictionary<string, string> dict = table.Rows.ToDictionary(r => r["Field"], r => r["Value"]);
+
+            url = envSettings.BaseUrl + "customers/api/CustomerSearch/?" + dict["parameter1"] + "=" + CheckForSpaces(dict["parameter2"]);
+            if (!string.IsNullOrEmpty(GetKey(dict, "parameter3")))
+            {
+                url = url + "&" + dict["parameter3"] + "=" + CheckForSpaces(dict["parameter4"]);
+            }
+            if (!string.IsNullOrEmpty(GetKey(dict, "parameter5")))
+            {
+                url = url + "&" + dict["parameter5"] + "=" + CheckForSpaces(dict["parameter6"]);
+            }
+
+            response = RestHelper.Get(url, envSettings.TouchPointId, envSettings.SubscriptionKey);
+        }
+
+
         [When(@"I get an Action by ID")]
         public void WhenIGetAnActionByID()
         {
             url = envSettings.BaseUrl + "actions/api/Customers/" + customerId + "/Interactions/" + interactionId + "/ActionPlans/" + actionPlanId + "/actions/" + actionId;
             response = RestHelper.Get(url, envSettings.TouchPointId, envSettings.SubscriptionKey);
         }
+
 
         [When(@"I get an ActionPlan by ID")]
         public void WhenIGetAnActionPlanByID()
@@ -350,16 +361,45 @@ namespace FunctionalTests.StepDefs
         [Then(@"the response body should contain:")]
         public void ThenMyBindingShouldHaveTheFollowingObjects(Table table)
         {
-            Dictionary<string, string> actualVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
             var expectedVals = new Dictionary<string, string>();
+            var actualVals = new Dictionary<string, string>();
+            actualVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
             foreach (var row in table.Rows)
             {
                 expectedVals.Add(row[0], row[1]);
             }
-            checkResults(expectedVals,actualVals).Should().Be(true);
+            checkResults(expectedVals, actualVals).Should().Be(true);
         }
 
-  
+
+        [Then(@"all returned Customers should contain:")]
+        public void ThenAllReturnedCustomersShouldContain(Table table)
+        {
+            var dictionary = ToDictionary(table);
+
+            var fName = GetKey(dictionary, "FamilyName");
+            var gName = GetKey(dictionary, "GivenName");
+            var uln = GetKey(dictionary, "UniqueLearnerNumber");
+            var dob = GetKey(dictionary, "DateOfBirth");
+
+            List<Customer> customers = JsonConvert.DeserializeObject<List<Customer>>(response.Content);
+
+            foreach (Customer c in customers)
+            {
+                if (!string.IsNullOrEmpty(gName)) { c.GivenName.ToLower().Contains(gName.ToLower()).Should().BeTrue(); }
+                if (!string.IsNullOrEmpty(fName)) { c.FamilyName.ToLower().Contains(fName.ToLower()).Should().BeTrue(); }
+                if (!string.IsNullOrEmpty(uln)) { c.UniqueLearnerNumber.ToLower().Contains(uln.ToLower()).Should().BeTrue(); }
+                if (!string.IsNullOrEmpty(dob)) { c.DateofBirth.ToLower().Contains(dob.ToLower()).Should().BeTrue(); }
+
+            }
+        }
+
+        public bool CheckFamilyName(string name)
+        {
+            return true;
+        }
+
+
 
         [Then(@"the error message should be ""(.*)""")]
         public void ThenTheErrorMessageShouldBe(string expectedMessage)
@@ -367,6 +407,23 @@ namespace FunctionalTests.StepDefs
             response.Content.Should().Contain(expectedMessage);
         }
 
+        [When(@"I post a Customer with '(.*)' and  '(.*)' and '(.*)' and '(.*)'")]
+        public void WhenIPostACustomerWithAndAndAnd(string GivenName, string FamilyName, string DateofBirth, string UniqueLearnerNumber)
+        {
+            url = envSettings.BaseUrl + "customers/api/customers/";
+            var customer = new Customer();
+            customer.GivenName = GivenName;
+            customer.FamilyName = FamilyName;
+            customer.DateofBirth = DateofBirth;
+            customer.UniqueLearnerNumber = UniqueLearnerNumber;
+            json2 = JsonConvert.SerializeObject(customer);
+            response = RestHelper.Post(url, json2, envSettings.TouchPointId, envSettings.SubscriptionKey);
+            if (response.IsSuccessful)
+            {
+                Dictionary<string, string> actualVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
+                customerId = actualVals["CustomerId"];
+            }
+        }
 
 
         private bool checkResults(Dictionary<string, string> expectedVals, Dictionary<string, string> actualVals)
@@ -385,5 +442,26 @@ namespace FunctionalTests.StepDefs
             return true;
         }
 
+        public static Dictionary<string, string> ToDictionary(Table table)
+
+        {
+            var dictionary = new Dictionary<string, string>();
+            foreach (var row in table.Rows)
+            {
+                dictionary.Add(row[0], row[1]);
+            }
+            return dictionary;
+        }
+
+        public static string CheckForSpaces(string s)
+        {
+            string[] words = s.Split('+');
+            return words[0];
+        }
+
+        private static string GetKey(IReadOnlyDictionary<string, string> dictValues, string keyValue)
+        {
+            return dictValues.ContainsKey(keyValue) ? dictValues[keyValue] : "";
+        }
     }
 }
