c3a76cacc61d7014c6cb4efafa7e8ba5fc210afb
Simon Wren <simon.j.wren@outlook.com>
2019-08-01 07:23:12 +0100

    Add new feature files, and initial tests for Diversity V3 Post

---
+++ NCS.DSS.FunctionalTests/Features/Diversity/DiversityV3_Post.feature.cs
@@ -0,0 +1,2 @@
+﻿#error (411:1): Feature file already contains a scenario with name 'Post Diversity with invalid values for DateAndTimeLDDHealthConsentCollected'
+#error (411:1): Scenario Outline 'Post Diversity with invalid values for DateAndTimeLDDHealthConsentCollected' has no examples defined
